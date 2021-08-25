be56dc9f334e39a347ad169c55fd7f38ab867174
MATSKEVICH_OLGA <Pola221182@list.ru>
2021-08-25 11:47:05 +0300

    menu

---
+++ menu2508/js/script.js
@@ -0,0 +1,8 @@
+
+    //burger handler
+    $(function() {
+        $('#nav-icon3').click(function(){
+            $(this).toggleClass('open');
+            $('.menu').toggleClass('dflex');
+        }); 
+    });
